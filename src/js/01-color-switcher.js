b5e8f741343c83c33a1deb7dacb640713d8beeec
NinaSlominska <nusya.slominskaya@gmail.com>
2022-12-27 21:13:17 +0200

    task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,21 @@
-// const btnStart
-// const  
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+const btnStart=document.querySelector('[data-start]')
+const btnStop = document.querySelector('[data-stop]')
+const body = document.querySelector('body')
+let timerId = null;
+btnStart.addEventListener('click', changColor)
+btnStop.setAttribute("disabled", '');
+function changColor(event) {
+   timerId = setInterval(() => {
+    body.style.backgroundColor=getRandomHexColor();
+   }, 1000);
+    btnStart.setAttribute("disabled", '');
+    btnStop.removeAttribute("disabled")
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    btnStart.removeAttribute("disabled")
+ btnStop.setAttribute("disabled", '');
+});
